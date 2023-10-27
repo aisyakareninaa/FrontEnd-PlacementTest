5af6c93b9c9f6bc555bc5ba6241f01e7ff5f7fba
Aisya Karenina <100953845+aisyakareninaa@users.noreply.github.com>
2023-10-27 19:57:03 +0700

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ title, description, img }) => {
                 <div className="text-black flex items-center mb-2">
                     <span className="font-bold text-xl">{title}</span>
                     {description && <span className="mx-2">|</span>}
-                    <span className="text-base mx-2">{description || "No description available."}</span>
+                    <span className="text-base mx-2 text-gray-500">{description || "| No description available."}</span>
                 </div>
             </div>
         </div>
